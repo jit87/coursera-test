dd55aa71244063db626bea75bf6b6ad16eb3a59f
Ignacio <ignaciotejada87@gmail.com>
2023-12-01 20:33:58 +0100

    upload project 2

---
+++ module1-solution/app.js
@@ -0,0 +1,36 @@
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope){
+   
+   $scope.dish = "";
+   var text1 = "Too much";
+   var text2 = "Enjoy";
+   $scope.textPrinted = "";
+
+   $scope.check = function (){
+        var count = 0; 
+        var elems = $scope.dish.split(","); 
+        
+        elems.forEach(element => {
+            count++; 
+        });
+        
+        if(count > 3){
+            $scope.textPrinted = text1;
+        }
+        else {
+            $scope.textPrinted = text2;
+        }
+    }
+
+    $scope.checkCount = function (){ 
+           return $scope.textPrinted;
+    }
+
+
+}
+
