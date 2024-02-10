0e8a80ba6bcbfd6241dd774c9b7cc4c0b4adcfe9
Ignacio <ignaciotejada87@gmail.com>
2024-02-10 01:44:31 +0100

    upload 5

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,18 +1,41 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService',  MenuSearchService);
+.service('MenuSearchService',  MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
 
 //Controllers
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService){
+    var list = this; 
     
 }
 
-//Directive
+//Directives
+function FoundItemsDirective(){
+    var ddo = {
+        templateURL: 'foundItemsDirective.html',
+        scope: {
+            items: '<',
 
+            onRemove : '&'
+        },
+        controller: NarrowItDownController,
+        controllerAs:'list',
+
+    }
+}
 
 
 //Service
 function  MenuSearchService(){
-    //getMatchedMenuItems(searchTerm);
+    getMatchedMenuItems(searchTerm)
+    {
+       /*return $http(...).then(function (result) {
+            // process result and only keep items that match
+            var foundItems...
+        
+            // return processed items
+            return foundItems;
+        });*/
+    }
 }
